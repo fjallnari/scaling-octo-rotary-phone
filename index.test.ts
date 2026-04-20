6629e47d6da5cd7c4a6dfe3392f6574da79aba1b
fjallnari <draconirx@gmail.com>
2026-04-20 20:23:59 +0200

    Add sum negative test

--- index.test.ts
+++ index.test.ts
@@ -4,3 +4,7 @@ import { add } from './index';
 test('add returns the sum of two numbers', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test('add returns a negative number when the sum is negative', () => {
+  expect(add(-1, -2)).toBe(-3);
+});
